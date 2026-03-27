f4c03130922d5b6d98c10de4857c7c64df55c7eb
qamariahzee <azizahnurqamariah0904@gmail.com>
2026-03-28 03:05:54 +0800

    memperbaiki login karena lupa menambahkan BASE_URL

--- app/auth/login.php
+++ app/auth/login.php
@@ -9,6 +9,7 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
+require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../config/koneksi.php';
 
 // Jika sudah login, redirect ke dashboard
